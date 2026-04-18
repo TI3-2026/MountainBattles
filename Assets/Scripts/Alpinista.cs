a6a199ee9213b367fda5ba1251d4867f0a1924e0
PitiedHarp <luiz12ferro@gmail.com>
2026-04-17 22:43:47 -0300

    Alpinista

---
+++ Assets/Scripts/Alpinista.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Alpinista : MonoBehaviour
+{
+    public GameObject alpinista;
+    Vector3 alpinistavtr;
+    Vector3 movimento = new Vector3(0, 6, 0);
+    Vector3 inicio = new Vector3(-4, -24, 100);
+    public int Erro = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        alpinistavtr = inicio;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Erro++;
+            ErrouCarta(ref Erro);
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            AcertouCarta();
+        }
+    }
+
+    public void AcertouCarta()
+    {
+        if(alpinistavtr.y < 20)
+        {
+            alpinistavtr = alpinistavtr + movimento;
+            alpinista.transform.position = alpinistavtr;
+        }
+    }
+    public void ErrouCarta(ref int erro)
+    {
+        if(alpinistavtr.y > inicio.y && erro == 1)
+        {
+            alpinista.transform.position = alpinistavtr + Vector3.down/2;
+        }
+        else if(alpinistavtr.y > inicio.y && erro == 2)
+        {
+            alpinista.transform.position = alpinistavtr + Vector3.down/2 + Vector3.down/2;
+        }
+        else if (alpinistavtr.y > inicio.y && erro >= 3)
+        {
+            alpinistavtr = alpinistavtr - movimento;
+            alpinista.transform.position = alpinistavtr;
+            erro = 0;
+        }
+    }
+
+}
