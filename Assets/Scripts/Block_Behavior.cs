d84cb8bc64a6630d573e90e8daeacf883c063ffa
Bernardo O. Horta <bernardo.o.horta@gmail.com>
2026-04-17 22:55:42 -0300

    Generate Block ID

--- Assets/Scripts/Block_Behavior.cs
+++ Assets/Scripts/Block_Behavior.cs
@@ -4,17 +4,28 @@ using UnityEngine.UI;
 
 public class Block_Behavior : MonoBehaviour
 {
-    private Image img;
+    [Header("Dont Touch")]
+    public int block_value;
+
+    [Header("References")]
+    public Image img;
 
     private void Start()
     {
-        Canvas canvas = GetComponentInParent<Canvas>();
-        canvas.worldCamera = Camera.main;
-        img = GetComponentInParent<Image>();
+        //Canvas canvas = GetComponentInParent<Canvas>();
+        //canvas.worldCamera = Camera.main;
+        
+        //img = GetComponentInParent<Image>();
     }
 
+    /*
     public void ChangeImage(Sprite sprite)
     {
         img.sprite = sprite;
+    }*/
+
+    public void ClickBlock()
+    {
+        Debug.Log("oi");
     }
 }
--- Assets/Scripts/Managers/Manager_Level.cs
+++ Assets/Scripts/Managers/Manager_Level.cs
@@ -8,10 +8,22 @@ public class Manager_Level : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private GameObject block_position_origin;
-    [SerializeField] private float block_row_offset;
-    [SerializeField] private int blocks_row;
-    [SerializeField] private float block_column_offset;
-    [SerializeField] private int blocks_column;
+
+    // Generation Variables
+    private float block_row_offset = 3.4f;
+    private int blocks_row = 3;
+    private float block_column_offset = 1.7f;
+    private int blocks_column = 6;
+
+    private int duos_blocks;
+    private int[] block_values;
+
+    // Level Control Variables
+
+    private int first_block_value;
+    private int second_block_value;
+
+    // ==================== Functions ====================
 
     private void Start()
     {
@@ -20,6 +32,33 @@ public class Manager_Level : MonoBehaviour
 
     private void GenerateLevel()
     {
+        duos_blocks = blocks_column * blocks_row / 2;
+        block_values = new int[blocks_column * blocks_row];
+        
+        // Generate block values
+        for (int i = 0; i < block_values.Length/2; i++){
+            block_values[i*2] = i;
+            block_values[i*2 + 1] = i;
+        }
+        // Shuffle block values
+        for (int i = 0; i < block_values.Length; i++){
+            int random_index = Random.Range(0, block_values.Length);
+            int temp = block_values[i];
+            block_values[i] = block_values[random_index];
+            block_values[random_index] = temp;
+        }
+
+        /* For Debugging:
+
+        string text = "";   
+        for (int i = 0; i < block_values.Length; i++){
+            text += block_values[i] + " ";
+        }
+        Debug.Log(text);
+        */
+        
+        // Create blocks
+        int block_index = 0;
         for (int i = 0; i < blocks_column; i++)
         {
             for (int j = 0; j < blocks_row; j++)
@@ -27,8 +66,16 @@ public class Manager_Level : MonoBehaviour
                 Vector3 block_position = new Vector3(i * block_column_offset, 0, j * block_row_offset);
 
                 GameObject block = Instantiate(prefab_MemoryBlock, block_position_origin.transform);
+                Block_Behavior block_behavior = block.GetComponent<Block_Behavior>();
+
                 block.transform.localPosition = block_position;
+                block_behavior.block_value = block_values[block_index];
+                block_index++;
             }
         }
+    }
+
+    public void BlockClicked() {
+        
     }
 }
